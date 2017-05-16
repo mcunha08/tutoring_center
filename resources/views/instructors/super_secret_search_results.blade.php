fa0798e2b5b263da44e32d297dd9bf871be1fc2b
Michael Cunha <Michael Cunha>
2017-05-15 18:34:56 -0700

    Finished super secret page

--- resources/views/instructors/super_secret_search_results.blade.php
+++ resources/views/instructors/super_secret_search_results.blade.php
@@ -5,7 +5,7 @@
        {{ $type }} search results for {{ $search_string }}
        <ul>
            @foreach($results as $result)
-                <li>{{ $result->lastname }} {{ $result->firstname }}. Email: {{ $result->email}} Location: {{ $result->location }} @if ($role_id == App\Role::where('name', 'Tutor')->first()->id) Rating: {{ $result->rating }}@endif</li>
+                <li>{{ $result->lastname }} {{ $result->firstname }}. Email: {{ $result->email }} Location: {{ $result->location }} @if ($role_id == App\Role::where('name', 'Tutor')->first()->id) Rating: {{ $result->rating }}@endif</li>
            @endforeach
        </ul>
 @endsection
